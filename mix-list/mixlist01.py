9217a0670268da22fa837c183e0bb32238884236
Ram <RRgitit@users.noreply.github.com>
2022-05-24 19:59:17 +0000

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# create a list containing three items
+# my_list = [ "192.168.0.5", 5060, "UP" ]
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+print("The IPs in the list are: " + iplist[3] + " and " + iplist[4])
+
